292d972964d83b6542be92b109314d244f3545df
MYK0128 <141742331+MYK0128@users.noreply.github.com>
2024-09-26 10:09:23 +0900

    Add files via upload

---
+++ 01_2024-09-26/sample01-21.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+
+    $data = 3;
+
+    for ($cnt = 1; $cnt <= 10; $cnt++) {
+        print $cnt;
+        if ($cnt <= 5 and $cnt <= $data) {
+            print "●";
+        }
+        if ($cnt >= 8 or $cnt <= $data) {
+            print "○";
+        }
+        if ($cnt <= 8 and $cnt <= $data) {
+            print "▼";
+        }
+        if (! ($cnt <= 5 and $cnt <= $data)) {
+            print "▽";
+        }
+        print "<br>";
+    }
+?>
+</body>
+</html>
