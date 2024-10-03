dfa5f0a56921c9c82de5214e4dd1c01219f9162b
harutiro <harutiro2027@outlook.jp>
2024-10-03 09:22:49 +0900

    問題11を完成

--- 02_2024-10-02/sample-02-11.php
+++ 02_2024-10-02/sample-02-11.php
@@ -1,1 +1,25 @@
-<?php echo "Hello, World!"; ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<body>
+    <?php
+    
+    mt_srand();
+
+    for($cnt = 1; $cnt <= 10; $cnt++){
+        $imagefile = mt_rand(1, 9);
+
+        $imagefile = "image" . $imagefile . ".gif";
+
+        print "<img src='./images/$imagefile' hspace='2'>";
+    }
+
+    ?>
+</body>
+
+</html>
