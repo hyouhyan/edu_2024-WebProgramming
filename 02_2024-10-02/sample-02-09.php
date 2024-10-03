41e8fee9c32ec93f7a346a4103dacfc3726e75b0
Hyouhyan <hyouhyan@hyouhyan.com>
2024-10-03 09:23:06 +0900

    add: sample-02-09

--- 02_2024-10-02/sample-02-09.php
+++ 02_2024-10-02/sample-02-09.php
@@ -1,1 +1,20 @@
-<?php echo "Hello, World!"; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<body>
+<?php
+    // 乱数ジェネレータを初期化
+    mt_srand();
+
+    // 1から6までの乱数を5回生成
+    for ($cnt = 1; $cnt <= 5; $cnt++) {
+        print mt_rand(1, 6);
+        print "<br>";
+    }
+?>
+</body>
+</html>
