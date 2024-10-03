a8252c3298068f236f438369bb111871334bcb2e
Hyouhyan <hyouhyan@hyouhyan.com>
2024-10-03 09:22:58 +0900

    add: sample-02-01

--- 02_2024-10-02/sample-02-01.php
+++ 02_2024-10-02/sample-02-01.php
@@ -1,1 +1,34 @@
-<?php echo "Hello, World!"; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<body>
+<?php
+    $data1 = 10;
+    $data2 = 3;
+
+    // 足し算
+    print $data1 + $data2;
+    print "<br>";
+
+    // 引き算
+    print $data1 - $data2;
+    print "<br>";
+
+    // 掛け算
+    print $data1 * $data2;
+    print "<br>";
+
+    // 割り算
+    print $data1 / $data2;
+    print "<br>";
+
+    // 余り
+    print $data1 % $data2;
+    print "<br>";
+?>
+</body>
+</html>
