1ca4c658c4520b1ec7bc87a0ab31ca99cc5df1d5
grkn7 <77403282+grkn7@users.noreply.github.com>
2024-10-03 09:45:52 +0900

    02-02のphp
    これできとる？

--- 02_2024-10-02/sample-02-02.php
+++ 02_2024-10-02/sample-02-02.php
@@ -1,1 +1,39 @@
-<?php echo "Hello, World!"; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<?php
+
+    $result = 0;        //初期化
+
+    //足し算
+    $result += 5;       //0+5=5が計算される
+    print $result;
+    print "<br>";
+
+    //引き算
+    $result -= 3;       //5-3=2が計算される
+    print $result;
+    print "<br>";
+
+    //掛け算
+    $result *= 10;      //2x10=20が計算される
+    print $result;
+    print "<br>";
+
+    //割り算
+    $result /= 2;       //20÷2=10が計算される
+    print $result;
+    print "<br>";
+
+    //余り
+    $result %= 4;       //10÷4の余り=2が計算される
+    print $result;
+    print "<br>";
+
+    ?>
+    </body>
+    </html>
