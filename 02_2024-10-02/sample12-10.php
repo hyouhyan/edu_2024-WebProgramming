698dc8d0fff35f718948f61b497332b524e317b6
nanairo <136669608+nanairo2743@users.noreply.github.com>
2024-10-03 09:50:49 +0900

    Add files via upload

---
+++ 02_2024-10-02/sample12-10.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<for lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?PHP 
+        if (isset($_POST['btnExec'])) {
+            for ($num = 1; $num <= 4; $num++) {
+                if (isset($_POST["inputdata$num"])){
+                    print "$num ヶめのチェックボックスはON<br>";
+                }
+                else {
+                    print "$num ヶめのチェックボックスはOFF<br>";
+                }
+            }
+            print "<hr><hr>";
+        }
+         ?>
+    開発経験のある言語にチェックをつけて[送信]ボタンをクリックしてください（複数選択可）。
+    <form action="?php $_SERVER['SCRIPT_NAME']?" method="post">
+        <p><input type="checkbox" name="inputdata1">PHP</p>
+        <p><input type="checkbox" name="inputdata2">Java</p>
+        <p><input type="checkbox" name="inputdata3">CGI</p>
+        <p><input type="checkbox" name="inputdata4">C++</p> 
+        <input type="submit" name="btnExec" value="送信">
+    </form>
+    </body>
+</html>
