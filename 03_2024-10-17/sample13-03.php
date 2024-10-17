18d1892766d8cb35a5266ae35902d69bc10c61bf
Icarus <icaruslove28@i.softbank.jp>
2024-10-17 09:56:39 +0900

    Add files via upload

---
+++ 03_2024-10-17/sample13-03.php
@@ -0,0 +1,23 @@
+<?php
+
+    //クッキーを削除
+    setcookie("mycookiedata");
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        //クッキーを読み込み
+        if (isset($_COOKIE['mycookiedata'])) {
+            //クッキーにデータがない時
+            print "データはクッキーに保存されていません!";
+        }
+
+        ?>
+    </body>
+</html>
