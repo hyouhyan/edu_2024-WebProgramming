857681eac4d2fcd5d1c7a1acabc5c59eec9c404b
Hyouhyan <hyouhyan@hyouhyan.com>
2024-10-17 09:36:35 +0900

    add: sample14-09.php

---
+++ 03_2024-10-17/sample14-09.php
@@ -0,0 +1,30 @@
+<? php
+    if (isset($_POST['btnExec'])){
+        if ($_POST['password'] == "12345"){
+            session_start();
+
+            $_SESSION['sesuserid'] = $_POST['userid'];
+            header("location: sample14-09-2.php");
+            exit();
+        }else{
+            print "パスワードが間違っています！<br><br>";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+ユーザーIDとパスワードを入力して[送信]ボタンをクリックしてください。
+<form action="<?php $_SERVER['SCRIPT_NAME']?>" method="POST">
+    ユーザーID：<input size="20" type="text" name="userid"><br>
+    <br>
+    パスワード：<input size="15" type="password" name="password">
+    <input type="submit" name="btnExec" value="送信">
+</form>
+
+</body>
+</html>
