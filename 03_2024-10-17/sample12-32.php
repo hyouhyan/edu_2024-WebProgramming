3287fa4c6a21ca1c3b2d29247f6ae69d4344faef
Hyouhyan <hyouhyan@hyouhyan.com>
2024-10-23 23:37:08 +0900

    fix: sample12-32の自分自身から呼び出されifを修正

--- 03_2024-10-17/sample12-32.php
+++ 03_2024-10-17/sample12-32.php
@@ -10,7 +10,7 @@
     if(isset($_SERVER['HTTP_REFERER'])){
         print "このPHPファイルは、次のURLから呼び出されました。<br>";
         print $_SERVER['HTTP_REFERER']. "<br><br>";
-        if($_SERVER['HTTP_REFERER'] == "http://localhost/03/sample12-32.php"){
+        if($_SERVER['HTTP_REFERER'] == "http://localhost/03/sample12-32.php" || $_SERVER['HTTP_REFERER'] == "http://localhost/03_2024-10-17/sample12-32.php"){
             print "自分自身のファイルから呼ばれました！<BR>";
         }
     }
