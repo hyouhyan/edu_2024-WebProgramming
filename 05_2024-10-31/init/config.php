831ed318cb376325adc52527c60072bc276b4237
Hyouhyan <hyouhyan@hyouhyan.com>
2024-10-28 16:47:35 +0900

    add: 05/init/config.php

---
+++ 05_2024-10-31/init/config.php
@@ -0,0 +1,25 @@
+<?php
+// ===============================================
+// オンラインリンク 設定ファイル
+// ===============================================
+// プログラム定数
+define("DEFSCR", "log");	// デフォルト画面
+define("AUTHADMIN", "1");	// 管理者セッション識別用
+
+// ユーザ設定用
+define("APPNAME", "オンラインリンク");	// アプリケーション名
+define("PAGESIZE", "10");			// 1ページ表示数
+define("ENCDISP", "UTF-8");			// 表示文字コード
+define("ENCDB", "UTF-8");			// データベース文字コード
+
+// 管理者用設定
+define("ADMINAPPNAME", "リンク管理");	// アプリケーション名
+define("ADMINPAGESIZE", "10");		// 1ページ表示数
+define("LOGINID", "testid");		// ログインID(要変更)
+define("LOGINPASS", "testpass");	// パスワード(要変更)
+
+// データベース設定
+define("DBSV", "localhost");		// DBホスト
+define("DBNAME", "link");			// DBテーブル
+define("DBUSER", "root");			// DBユーザ名(要変更)
+define("DBPASS", "root");			// DBユーザパスワード(要変更)
